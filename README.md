923894575dd288ecc965a2081a0ca13c4420bdef
Angga Ari Wijaya <anggadarkprince@gmail.com>
2017-01-03 13:01:38 +0700

    add docjs and badges

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Learn TDD
 [![Build Status](https://travis-ci.org/anggadarkprince/learn-tdd.svg?branch=master)](https://travis-ci.org/anggadarkprince/learn-tdd)
 [![codecov](https://codecov.io/gh/anggadarkprince/learn-tdd/branch/master/graph/badge.svg)](https://codecov.io/gh/anggadarkprince/learn-tdd)
+[![Code Climate](https://codeclimate.com/github/anggadarkprince/learn-tdd/badges/gpa.svg)](https://codeclimate.com/github/anggadarkprince/learn-tdd)
+[![NSP Status](https://nodesecurity.io/orgs/sketch-project-studio/projects/90dfb5b9-d4d3-4864-9d65-138b3d017924/badge)](https://nodesecurity.io/orgs/sketch-project-studio/projects/90dfb5b9-d4d3-4864-9d65-138b3d017924)
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/anggadarkprince/learn-tdd/issues)
+[![Twitter Follow](https://img.shields.io/twitter/follow/espadrine.svg?style=social&label=Follow)](https://twitter.com/anggadarkprince)
+
 
 Test Driven Development and Behavior Driven Development in Javascript using MochaJS and Qunit for unit testing library and BlanketJS and InstanbulJS for code coverage.
 
--- src/js/user.js
+++ src/js/user.js
@@ -1,11 +1,26 @@
 var USERS = [];
 
+/** A module. Its name is module:foo/bar.
+ * @module User
+ */
+
+/**
+ * User object wrapper that contain user entity
+ * @param {string} name user full name
+ * @param {string} username id of user
+*/
 function User(name, username, email) {
     this.name = name;
     this.username = username;
     this.email = email;
 }
 
+/** An event. Its name is module:User.event:find.
+ * Find user and return its data position
+ * @param {string} username id of user
+ * @returns {number} position of data (-1 is not found)
+ * @event module:User.event:find
+ */
 User.prototype.find = function (username) {
     for (var i = 0; i < USERS.length; i++) {
         if (USERS[i].id === username) {
@@ -15,6 +30,12 @@ User.prototype.find = function (username) {
     return -1;
 };
 
+/** An event. Its name is module:User/age.event:get.
+ * Get user data and return as object
+ * @param {string} username id of user
+ * @returns {object} user data (null is not found)
+ * @event module:User.event:get
+ */
 User.prototype.get = function (username) {
     var result;
     if (username === null || username === undefined) {
@@ -29,6 +50,11 @@ User.prototype.get = function (username) {
     return null;
 };
 
+/** An event. Its name is module:User.event:save.
+ * Save current user to database
+ * @return {boolean} status of save operation
+ * @event module:User.event:save
+ */
 User.prototype.save = function () {
     USERS.push({
         id: this.username,
@@ -38,6 +64,11 @@ User.prototype.save = function () {
     return true;
 };
 
+/** An event. Its name is module:User.event:update.
+ * Update current user to database
+ * @return {boolean} status of save operation
+ * @event module:User.event:update
+ */
 User.prototype.update = function () {
     var exist = this.find(this.username);
     if (exist !== -1) {
@@ -51,6 +82,11 @@ User.prototype.update = function () {
     return false;
 };
 
+/** An event. Its name is module:User.event:delete.
+ * Delete current user to database
+ * @return {boolean} status of save operation
+ * @event module:User.event:delete
+ */
 User.prototype.delete = function () {
     var result = this.find(this.username);
     if (result !== -1) {
